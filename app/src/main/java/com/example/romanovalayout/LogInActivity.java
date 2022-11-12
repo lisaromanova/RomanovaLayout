76015f724711e89824615a1ff74ca7f89ae9b998
Elizaveta <soika-peresmeshnica2015@yandex.ru>
2022-11-13 01:42:50 +0300

    CommitLogIn

--- app/src/main/java/com/example/romanovalayout/LogInActivity.java
+++ app/src/main/java/com/example/romanovalayout/LogInActivity.java
@@ -2,13 +2,19 @@ package com.example.romanovalayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        EditText Password = findViewById(R.id.etPassword);
+        Drawable image = getResources().getDrawable(R.drawable.eyeslash);
+        Password.setCompoundDrawablesWithIntrinsicBounds(null, null, image, null);
     }
 }
