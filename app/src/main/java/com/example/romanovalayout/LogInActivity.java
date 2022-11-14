f64bc0eb6611b39e3bf20ed2b443e4a67a07be79
Elizaveta <soika-peresmeshnica2015@yandex.ru>
2022-11-14 21:49:36 +0300

    CommitLayout

--- app/src/main/java/com/example/romanovalayout/LogInActivity.java
+++ app/src/main/java/com/example/romanovalayout/LogInActivity.java
@@ -1,20 +1,82 @@
 package com.example.romanovalayout;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LogInActivity extends Activity {
+    EditText email, password;
+    Button logIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        EditText Password = findViewById(R.id.etPassword);
+        password = findViewById(R.id.etPassword);
+        email = findViewById(R.id.etNamePhone);
+        logIn= findViewById(R.id.btnLogin);
         Drawable image = getResources().getDrawable(R.drawable.eyeslash);
-        Password.setCompoundDrawablesWithIntrinsicBounds(null, null, image, null);
+        password.setCompoundDrawablesWithIntrinsicBounds(null, null, image, null);
+
+        TextView register = findViewById(R.id.txtRegister);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LogInActivity.this, RegisterActivity.class));
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                IsClear();
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                IsClear();
+            }
+        });
     }
+
+    void IsClear(){
+        if(!email.getText().toString().isEmpty()&&!password.getText().toString().isEmpty()){
+            logIn.setBackground(getResources().getDrawable(R.drawable.background_border_dark));
+            logIn.setTextColor(getResources().getColor(R.color.white));
+        }
+        else{
+            logIn.setBackground(getResources().getDrawable(R.drawable.background_border));
+            logIn.setTextColor(getResources().getColor(R.color.violetDark));
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/romanovalayout/RegisterActivity.java
@@ -0,0 +1,127 @@
+package com.example.romanovalayout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends Activity {
+    EditText password, confirmPassword, email, name;
+    CheckBox check;
+    Button reg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        email = findViewById(R.id.etNamePhoneReg);
+        name = findViewById(R.id.etFullName);
+        password = findViewById(R.id.etPasswordReg);
+        confirmPassword = findViewById(R.id.etConfirmPassword);
+        check = findViewById(R.id.checkAgree);
+        reg = findViewById(R.id.btnRegister);
+        Drawable image = getResources().getDrawable(R.drawable.eyeslash);
+        password.setCompoundDrawablesWithIntrinsicBounds(null, null, image, null);
+        confirmPassword.setCompoundDrawablesWithIntrinsicBounds(null, null, image, null);
+
+        TextView login = findViewById(R.id.txtLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LogInActivity.class));
+            }
+        });
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                IsClear();
+            }
+        });
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                IsClear();
+            }
+        });
+        confirmPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                IsClear();
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                IsClear();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                IsClear();
+            }
+        });
+        check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                IsClear();
+            }
+        });
+    }
+
+    void IsClear(){
+        if(!email.getText().toString().isEmpty()&&
+                !password.getText().toString().isEmpty()&&
+                !name.getText().toString().isEmpty()&&
+                !confirmPassword.getText().toString().isEmpty()&&
+        check.isChecked()){
+            reg.setBackground(getResources().getDrawable(R.drawable.background_border_dark));
+            reg.setTextColor(getResources().getColor(R.color.white));
+        }
+        else{
+            reg.setBackground(getResources().getDrawable(R.drawable.background_border));
+            reg.setTextColor(getResources().getColor(R.color.violetDark));
+        }
+    }
+}
